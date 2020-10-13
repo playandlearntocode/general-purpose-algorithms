84e7b634f527848bf9c205dd083e7d5063b291d2
playandlearntocode <playandlearntocode@gmail.com>
2020-10-13 09:30:58 +0200

    Minor reformat

--- integer-division/solution.py
+++ integer-division/solution.py
@@ -1,7 +1,7 @@
 class Solution:
-    MIN_INT = pow(-2,31)
-    MAX_INT = pow(2,31) - 1    
-    
+    MIN_INT = pow(-2, 31)
+    MAX_INT = pow(2, 31) - 1
+
     def recursiveDivide(self, currentDividend: int, currentDivisor: int):
         quotient = 1
         accumulator = currentDivisor  # same as currentDivisor * quotient because quotient == 1 at this point
@@ -10,36 +10,35 @@ class Solution:
             return 0
         elif currentDividend == currentDivisor:
             return 1
-        
-        while accumulator<currentDividend:
+
+        while accumulator < currentDividend:
             quotient = quotient << 1
-            accumulator =accumulator << 1 # implicit quotient inclusion here!
-        
+            accumulator = accumulator << 1  # implicit quotient inclusion here!
+
         # undo the last step, because accumulator is now larger than currentDividend
-        accumulator = accumulator >>1
-        quotient = quotient >> 1                
-        return quotient + self.recursiveDivide(currentDividend-accumulator, currentDivisor)
-    
-    def divide(self, dividend: int, divisor: int) -> int:    
+        accumulator = accumulator >> 1
+        quotient = quotient >> 1
+        return quotient + self.recursiveDivide(currentDividend - accumulator, currentDivisor)
+
+    def divide(self, dividend: int, divisor: int) -> int:
         # determine the sign of quotient:
-        negative = False        
+        negative = False
         if (dividend >= 0 and divisor >= 0):
             negative = False
-        elif (dividend<0 and divisor >=0):
+        elif (dividend < 0 and divisor >= 0):
             negative = True
-        elif (dividend >0 and divisor<=0):
+        elif (dividend > 0 and divisor <= 0):
             negative = True
-            
+
         # extract positive values of dividend and divisor:
         absDividend, absDivisor = abs(dividend), abs(divisor)
-        
+
         # watch for limits:
-        if (absDivisor==1):
-            if (negative == True):                
+        if (absDivisor == 1):
+            if (negative == True):
                 return -absDividend if Solution.MIN_INT < -absDividend else Solution.MIN_INT
             else:
                 return absDividend if Solution.MAX_INT > absDividend else Solution.MAX_INT
-        
+
         q = self.recursiveDivide(absDividend, absDivisor)
-        return q if negative == False else -q        
-            
+        return q if negative == False else -q
